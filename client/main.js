cff04adc759aeec84b93b8655946bceb8f01390e
Cirych <cir_reg@mail.ru>
2015-12-02 01:31:34 +0300

    Voting

--- client/main.js
+++ client/main.js
@@ -1,3 +1,4 @@
+/* global Websites */
 /* global Template */
 /* global Router */
 /* global $ */
@@ -17,6 +18,14 @@ Router.route('/', function () {
 		to: "main"
 	});
 });
+Router.route('/url/:_id', function () {
+	this.render('item', {
+		to: "main",
+		data() {
+			return Websites.findOne({_id:this.params._id});
+		}
+	});
+});
 
 /////
 // template helpers 
@@ -25,10 +34,21 @@ Router.route('/', function () {
 // helper function that returns all available websites
 Template.website_list.helpers({
 	websites(){
-		return Websites.find({});
+		return Websites.find({}, {sort:{upVoted: -1}});
 	}
 });
-
+Template.website_item.helpers({
+	getUser(user_id){
+		let user = Meteor.users.findOne({_id:user_id});
+		if(user)
+			return user.username;
+		else
+			return "anon";
+	},
+	isEnabled() {
+		return Meteor.user()?"":"disabled";
+	}
+});
 
 /////
 // template events 
@@ -36,40 +56,59 @@ Template.website_list.helpers({
 
 Template.website_item.events({
 	"click .js-upvote":function(event){
-		// example of how you can access the id for the website in the database
-		// (this is the data context for the template)
 		var website_id = this._id;
-		console.log("Up voting website with id "+website_id);
-		// put the code in here to add a vote to a website!
-
-		return false;// prevent the button from reloading the page
+		Websites.update({_id:website_id},{$inc:{upVoted:1}});
+		return false;
 	}, 
 	"click .js-downvote":function(event){
-
-		// example of how you can access the id for the website in the database
-		// (this is the data context for the template)
 		var website_id = this._id;
-		console.log("Down voting website with id "+website_id);
-
-		// put the code in here to remove a vote from a website!
-
-		return false;// prevent the button from reloading the page
+		Websites.update({_id:website_id},{$inc:{downVoted:1}});
+		return false;
 	}
 })
 
-Template.website_form.events({
+Template.welcome.events({
 	"click .js-toggle-website-form":function(event){
-		$("#website_form").toggle('slow');
+		$("#website_form").modal('show');
+		console.log('show modal add form');
 	},
 	"submit .js-save-website-form":function(event){
-
-		// here is an example of how to get the url out of the form:
-		var url = event.target.url.value;
-		console.log(`The url they entered is: ${url}`);
-		
-		//  put your website saving code in here!	
-
-		return false;// stop the form submit from reloading the page
-
+		if (Meteor.user()){
+		Websites.insert({
+    		title: event.target.title.value,
+    		url: event.target.url.value, 
+    		description: event.target.description.value,
+			upVoted: 0,
+			downVoted: 0,
+    		createdOn:new Date(),
+			createdBy:Meteor.user()._id
+    	});
+		}
+		$("#website_form").modal('hide');
+		return false;
 	}
 });
+
+///Global helpers
+
+Template.registerHelper('prettyDate', function(date) {
+	return moment(date).calendar();
+});
+
+/// accounts config
+
+Accounts.ui.config({
+passwordSignupFields: "USERNAME_AND_EMAIL"
+});
+
+Template.welcome.helpers({username:function(){
+if (Meteor.user()){
+  return Meteor.user().username;
+    //return Meteor.user().emails[0].address;
+}
+else {
+  return "anonymous internet user";
+}
+}
+});
+
--- server/server.js
+++ server/server.js
@@ -6,25 +6,33 @@
     	  Websites.insert({
     		title:"Goldsmiths Computing Department", 
     		url:"http://www.gold.ac.uk/computing/", 
-    		description:"This is where this course was developed.", 
+    		description:"This is where this course was developed.",
+        upVoted: 0,
+			downVoted: 0,
     		createdOn:new Date()
     	});
     	 Websites.insert({
     		title:"University of London", 
     		url:"http://www.londoninternational.ac.uk/courses/undergraduate/goldsmiths/bsc-creative-computing-bsc-diploma-work-entry-route", 
-    		description:"University of London International Programme.", 
+    		description:"University of London International Programme.",
+        upVoted: 0,
+			downVoted: 0,
     		createdOn:new Date()
     	});
     	 Websites.insert({
     		title:"Coursera", 
     		url:"http://www.coursera.org", 
-    		description:"Universal access to the world’s best education.", 
+    		description:"Universal access to the world’s best education.",
+        upVoted: 0,
+			downVoted: 0,
     		createdOn:new Date()
     	});
     	Websites.insert({
     		title:"Google", 
     		url:"http://www.google.com", 
-    		description:"Popular search engine.", 
+    		description:"Popular search engine.",
+        upVoted: 0,
+			downVoted: 0,
     		createdOn:new Date()
     	});
     }
